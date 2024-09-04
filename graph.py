512306c2bda5f0e63508ab76c30c68aa03339f0d
Mark Lawrenson <mark@lawrenson.id.au>
2024-09-04 21:10:15 +1000

    Janky WIP where the agent can modify the code.

--- graph.py
+++ graph.py
@@ -1,21 +1,18 @@
-from typing import Annotated, Type
+from typing import Annotated, Literal
 from typing_extensions import TypedDict
-from pydantic import BaseModel
 from langchain_anthropic import ChatAnthropic
-from langchain_core.tools import BaseTool
-from langchain_core.globals import set_debug, set_verbose
-from langgraph.graph import StateGraph
+from langchain_core.tools import tool
+from langgraph.graph import StateGraph, MessagesState
 from langgraph.graph.message import add_messages
-from langgraph.prebuilt import ToolNode, tools_condition
+from langgraph.prebuilt import ToolNode
 from langgraph.checkpoint.memory import MemorySaver
 import importlib.util
 import sys
-import ast
 
 import model
 
-set_debug(False)
-set_verbose(False)
+# set_debug(True)
+# set_verbose(True)
 
 memory = MemorySaver()
 
@@ -27,68 +24,66 @@ class State(TypedDict):
 graph_builder = StateGraph(State)
 
 
-class ReadModelTool(BaseTool):
-    name = "read_model"
-    description = "Read the timetable optimization model code"
+# Tool to modify the model code
+@tool
+def write_model(new_code: str) -> str:
+    """Write the timetable optimization model code."""
+    try:
+        # Parse the new code to check for syntax errors
+        # ast.parse(new_code)
 
-    def _run(self) -> str:
-        """Modify the model code."""
-        try:
+        # Write the new code to model.py
+        with open("model.py", "w") as f:
+            f.write(new_code)
 
-            # Write the new code to model.py
-            with open("model.py", "r") as f:
-                code = f.read()
+        # Reload the module
+        if "model" in sys.modules:
+            print("DEBUG: Model module found in sys.modules, reloading it.")
+            # Remove the module from the system cache to ensure it's reloaded
+            del sys.modules["model"]
+        spec = importlib.util.spec_from_file_location("model", "model.py")
+        module = importlib.util.module_from_spec(spec)
+        sys.modules["model"] = module
+        spec.loader.exec_module(module)
 
-            return code
-        except Exception as e:
-            return f"Error reading the model: {str(e)}"
-
-
-class ModifyModelTool(BaseTool):
-    name = "modify_model"
-    description = "Modify the timetable optimization model code"
-
-    class ModifyModelInput(BaseModel):
-        new_code: str
-
-    args_schema: Type[BaseModel] = ModifyModelInput
-
-    def _run(self, new_code: str) -> str:
-        """Modify the model code."""
-        try:
-            # Parse the new code to check for syntax errors
-            ast.parse(new_code)
-
-            # Write the new code to model.py
-            with open("model.py", "w") as f:
-                f.write(new_code)
-
-            # Reload the module
-            spec = importlib.util.spec_from_file_location("model", "model.py")
-            module = importlib.util.module_from_spec(spec)
-            sys.modules["model"] = module
-            spec.loader.exec_module(module)
-
-            return "Model successfully modified and reloaded."
-        except SyntaxError as e:
-            return f"Syntax error in the new code: {str(e)}"
-        except Exception as e:
-            return f"Error modifying the model: {str(e)}"
+        return "Model successfully written and reloaded."
+    except SyntaxError as e:
+        return f"Syntax error in the new code: {str(e)}"
+    except Exception as e:
+        return f"Error modifying the model: {str(e)}"
 
 
-class TimetableOptimiserTool(BaseTool):
-    name = "time_table_optimiser"
-    description = "Optimise the timetable"
-    args_schema: Type[BaseModel] = model.TimetableInputSchema
+# Tool to read the model code
+@tool
+def read_model() -> str:
+    """Read the current timetable optimization model code."""
+    try:
+        with open("model.py", "r") as f:
+            code = f.read()
+        return code
+    except Exception as e:
+        return f"Error reading the model: {str(e)}"
 
-    def _run(self, input: model.TimetableInput) -> str:
-        """Use the tool."""
+
+# Tool to optimize the timetable
+@tool(args_schema=model.TimetableInputSchema)
+def time_table_optimiser(input: model.TimetableInput) -> str:
+    """Optimise the timetable"""
+    try:
+        import model as model_run
+
         data = input.model_dump()
-        return model.create_and_solve_timetable_model(data)
+        return model_run.create_and_solve_timetable_model(data)
+    except Exception as e:
+        return f"Error optimizing the timetable: {str(e)}. input was {input}"
 
 
-tools = [TimetableOptimiserTool(), ModifyModelTool(), ReadModelTool()]
+# Now bind these functions to ToolNodes
+tools = [read_model, write_model, time_table_optimiser]
+tool_node = ToolNode(tools)
+
 llm = ChatAnthropic(model="claude-3-5-sonnet-20240620")
+# llm = ChatOpenAI(model="gpt-4o")
 llm_with_tools = llm.bind_tools(tools)
 
 
@@ -96,16 +91,24 @@ def chatbot(state: State):
     return {"messages": [llm_with_tools.invoke(state["messages"])]}
 
 
-graph_builder.add_node("chatbot", chatbot)
+def should_continue(state: MessagesState) -> Literal["tools", "__end__"]:
+    messages = state["messages"]
+    last_message = messages[-1]
+    if last_message.tool_calls:
+        return "tools"
+    return "__end__"
 
-tool_node = ToolNode(tools=tools)
+
+graph_builder.add_node("chatbot", chatbot)
 graph_builder.add_node("tools", tool_node)
+
 
 graph_builder.add_conditional_edges(
     "chatbot",
-    tools_condition,
+    should_continue,
 )
 graph_builder.add_edge("tools", "chatbot")
+
 graph_builder.set_entry_point("chatbot")
 graph = graph_builder.compile(checkpointer=memory)
 
@@ -116,6 +119,12 @@ if __name__ == "__main__":
         if user_input.lower() in ["quit", "exit", "q"]:
             print("Goodbye!")
             break
-        for event in graph.stream({"messages": ("user", user_input)}, config=config):
-            for value in event.values():
-                print("Assistant:", value["messages"][-1].content)
+        for event in graph.stream({"messages": [("user", user_input)]}, config=config):
+            for key, value in event.items():
+                if key == "chatbot":
+                    print("Assistant:", value["messages"][-1].content)
+                elif key == "tools":
+                    if value["messages"][-1].name != "read_model":
+                        print("Tool Result:", value["messages"][-1].content)
+                    else:
+                        print("Tool Result: Model read.")
--- model.py
+++ model.py
@@ -1,24 +1,44 @@
+# ALWAYS MODIFY THIS CODE IN COMPLETE FORM, NEVER WRITE PLACEHOLDERS.
+# THIS CODE MUST BE RUN DIRECTLY AS WRITTEN.
+
 from pyomo.environ import *
 from prettytable import PrettyTable
 from pydantic import BaseModel
 from typing import List, Optional, Literal
+from pyomo.contrib.appsi.solvers import Highs
 
-
-# Define the literal types
-ClassLiteral = Literal["C1", "C2", "C3", "C4", "C5"]
-TimeSlotLiteral = Literal["T1", "T2", "T3", "T4", "T5"]
-TeacherLiteral = Literal["Teacher1", "Teacher2", "Teacher3", "Teacher4"]
+# Update the literal types to include C26
+ClassLiteral = Literal[
+    "C1",
+    "C2",
+    "C3",
+    "C4",
+    "C5",
+    "C6",
+    "C7",
+    "C8",
+    "C9",
+    "C10",
+    "C11",
+    "C12",
+    "C13",
+    "C14",
+    "C15",
+    "C16",
+    "C17",
+    "C18",
+    "C19",
+    "C20",
+    "C21",
+    "C22",
+    "C23",
+    "C24",
+    "C25",
+    "C26",
+]
+TimeSlotLiteral = Literal["T1", "T2", "T3", "T4", "T5", "T6", "T7", "T8", "T9", "T10"]
+TeacherLiteral = Literal["Teacher1", "Teacher2", "Teacher3", "Teacher4", "Teacher5"]
 ClassroomLiteral = Literal["Room1", "Room2", "Room3", "Room4"]
-
-
-class ClassTeacherMapping(BaseModel):
-    classId: ClassLiteral
-    teacher: TeacherLiteral
-
-
-class ClassRoomMapping(BaseModel):
-    classId: ClassLiteral
-    room: ClassroomLiteral
 
 
 class ForcedAssignment(BaseModel):
@@ -37,8 +57,6 @@ class TimetableInput(BaseModel):
     TimeSlots: List[TimeSlotLiteral]
     Teachers: List[TeacherLiteral]
     Classrooms: List[ClassroomLiteral]
-    ClassTeacherMapping: List[ClassTeacherMapping]
-    ClassRoomMapping: List[ClassRoomMapping]
     Constraints: Constraints
     ForcedAssignments: Optional[List[ForcedAssignment]] = None
 
@@ -47,27 +65,83 @@ class TimetableInputSchema(BaseModel):
     input: TimetableInput
 
 
+# Create a model
+model = ConcreteModel()
+
+
 def create_and_solve_timetable_model(data):
+    # IMPORTANT: TO MODIFY ENSURE YOU WRITE THE MODEL MATHEMATICALLY, MAKE CHANGES MATHEMATICALLY, ENSURE THE MODEL IS EFFICIENT MATHEMATICALLY THEN IMPLEMENT.
+    # ALWAYS WRITE THE CURRENT MATHEMATICAL MODEL HERE:
+    # Mathematical Model:
+    # Sets:
+    # C: Set of classes
+    # T: Set of time slots
+    # E: Set of teachers
+    # R: Set of classrooms
+
+    # Decision Variables:
+    # x[c,t] = 1 if class c is assigned to time slot t, 0 otherwise
+    # y[e,t] = 1 if teacher e is teaching in time slot t, 0 otherwise
+    # z[c,t,e] = 1 if class c is assigned to time slot t and teacher e, 0 otherwise
+    # w[c,t,r] = 1 if class c is assigned to time slot t and classroom r, 0 otherwise
+    # teacher_assignment[c,e] = 1 if teacher e is assigned to class c, 0 otherwise
+    # room_assignment[c,r] = 1 if classroom r is assigned to class c, 0 otherwise
+    # first[e]: First time slot for teacher e
+    # last[e]: Last time slot for teacher e
+
+    # Objective:
+    # Minimize sum(last[e] - first[e] for e in E)
+
+    # Constraints:
+    # 1. Each class must be assigned exactly one time slot:
+    #    sum(x[c,t] for t in T) = 1 for all c in C
+    # 2. Each class must be assigned exactly one teacher:
+    #    sum(teacher_assignment[c,e] for e in E) = 1 for all c in C
+    # 3. Each class must be assigned exactly one room:
+    #    sum(room_assignment[c,r] for r in R) = 1 for all c in C
+    # 4. No teacher conflicts:
+    #    sum(z[c,t,e] for c in C) <= 1 for all t in T, e in E
+    # 5. No room conflicts:
+    #    sum(w[c,t,r] for c in C) <= 1 for all t in T, r in R
+    # 6. Linking constraints for z:
+    #    z[c,t,e] <= x[c,t] for all c in C, t in T, e in E
+    #    z[c,t,e] <= teacher_assignment[c,e] for all c in C, t in T, e in E
+    #    z[c,t,e] >= x[c,t] + teacher_assignment[c,e] - 1 for all c in C, t in T, e in E
+    # 7. Linking constraints for w:
+    #    w[c,t,r] <= x[c,t] for all c in C, t in T, r in R
+    #    w[c,t,r] <= room_assignment[c,r] for all c in C, t in T, r in R
+    #    w[c,t,r] >= x[c,t] + room_assignment[c,r] - 1 for all c in C, t in T, r in R
+    # 8. Linking x and y:
+    #    sum(z[c,t,e] for c in C) >= y[e,t] for all e in E, t in T
+    # 9. Defining first and last time slots for each teacher:
+    #    first[e] <= t + (1 - y[e,t]) * |T| for all e in E, t in T
+    #    last[e] >= t - (1 - y[e,t]) * |T| for all e in E, t in T
+    # ----------------------------------------
+    # ENSURE THE ABOVE MODEL AND THE CODE ARE COMPLETELY CONSISTENT.
+
     # Create a model
     model = ConcreteModel()
 
     # Sets
     model.Classes = Set(initialize=data["Classes"])
-    model.TimeSlots = Set(initialize=data["TimeSlots"])
+    model.TimeSlots = Set(
+        initialize=list(range(1, len(data["TimeSlots"]) + 1))
+    )  # Use indices
     model.Teachers = Set(initialize=data["Teachers"])
     model.Classrooms = Set(initialize=data["Classrooms"])
 
-    # Mappings
-    class_teacher = {
-        entry["classId"]: entry["teacher"] for entry in data["ClassTeacherMapping"]
-    }
-    class_room = {entry["classId"]: entry["room"] for entry in data["ClassRoomMapping"]}
-
     # Decision Variables
     model.x = Var(model.Classes, model.TimeSlots, domain=Binary)
+    model.teacher_assignment = Var(model.Classes, model.Teachers, domain=Binary)
+    model.room_assignment = Var(model.Classes, model.Classrooms, domain=Binary)
+    model.y = Var(model.Teachers, model.TimeSlots, domain=Binary)
+    model.first = Var(model.Teachers, within=NonNegativeIntegers)
+    model.last = Var(model.Teachers, within=NonNegativeIntegers)
+    model.z = Var(model.Classes, model.TimeSlots, model.Teachers, domain=Binary)
+    model.w = Var(model.Classes, model.TimeSlots, model.Classrooms, domain=Binary)
+    model.v = Var(model.Classes, model.TimeSlots, model.Teachers, domain=Binary)
 
     # Constraints
-
     # 1. Each class must be assigned exactly one time slot
     if data["Constraints"]["OneTimeSlotPerClass"]:
 
@@ -78,31 +152,108 @@ def create_and_solve_timetable_model(data):
             model.Classes, rule=one_time_slot_per_class_rule
         )
 
-    # 2. No two classes that share a common teacher can be in the same time slot
+    # 2. Each class must be assigned exactly one teacher
+    def one_teacher_per_class_rule(model, c):
+        return (
+            sum(model.teacher_assignment[c, teacher] for teacher in model.Teachers) == 1
+        )
+
+    model.one_teacher_per_class = Constraint(
+        model.Classes, rule=one_teacher_per_class_rule
+    )
+
+    # 3. Each class must be assigned exactly one room
+    def one_room_per_class_rule(model, c):
+        return sum(model.room_assignment[c, room] for room in model.Classrooms) == 1
+
+    model.one_room_per_class = Constraint(model.Classes, rule=one_room_per_class_rule)
+
+    # 4. No two classes that share a common teacher can be in the same time slot
     if data["Constraints"]["TeacherConflict"]:
 
         def teacher_conflict_rule(model, t, teacher):
-            classes_with_teacher = [
-                c for c in model.Classes if class_teacher[c] == teacher
-            ]
-            return sum(model.x[c, t] for c in classes_with_teacher) <= 1
+            return sum(model.z[c, t, teacher] for c in model.Classes) <= 1
 
         model.teacher_conflict = Constraint(
             model.TimeSlots, model.Teachers, rule=teacher_conflict_rule
         )
 
-    # 3. No two classes that share a common classroom can be in the same time slot
+    # Link z with x and teacher_assignment
+    def link_z_with_x_and_teacher_rule1(model, c, t, teacher):
+        return model.z[c, t, teacher] <= model.x[c, t]
+
+    model.link_z_with_x_and_teacher1 = Constraint(
+        model.Classes,
+        model.TimeSlots,
+        model.Teachers,
+        rule=link_z_with_x_and_teacher_rule1,
+    )
+
+    def link_z_with_x_and_teacher_rule2(model, c, t, teacher):
+        return model.z[c, t, teacher] <= model.teacher_assignment[c, teacher]
+
+    model.link_z_with_x_and_teacher2 = Constraint(
+        model.Classes,
+        model.TimeSlots,
+        model.Teachers,
+        rule=link_z_with_x_and_teacher_rule2,
+    )
+
+    def link_z_with_x_and_teacher_rule3(model, c, t, teacher):
+        return (
+            model.z[c, t, teacher]
+            >= model.x[c, t] + model.teacher_assignment[c, teacher] - 1
+        )
+
+    model.link_z_with_x_and_teacher3 = Constraint(
+        model.Classes,
+        model.TimeSlots,
+        model.Teachers,
+        rule=link_z_with_x_and_teacher_rule3,
+    )
+
+    # 5. No two classes that share a common classroom can be in the same time slot
     if data["Constraints"]["RoomConflict"]:
 
         def room_conflict_rule(model, t, room):
-            classes_in_room = [c for c in model.Classes if class_room[c] == room]
-            return sum(model.x[c, t] for c in classes_in_room) <= 1
+            return sum(model.w[c, t, room] for c in model.Classes) <= 1
 
         model.room_conflict = Constraint(
             model.TimeSlots, model.Classrooms, rule=room_conflict_rule
         )
 
-    # 4. Force specific classes into specific time slots
+    # Link w with x and room_assignment
+    def link_w_with_x_and_room_rule1(model, c, t, room):
+        return model.w[c, t, room] <= model.x[c, t]
+
+    model.link_w_with_x_and_room1 = Constraint(
+        model.Classes,
+        model.TimeSlots,
+        model.Classrooms,
+        rule=link_w_with_x_and_room_rule1,
+    )
+
+    def link_w_with_x_and_room_rule2(model, c, t, room):
+        return model.w[c, t, room] <= model.room_assignment[c, room]
+
+    model.link_w_with_x_and_room2 = Constraint(
+        model.Classes,
+        model.TimeSlots,
+        model.Classrooms,
+        rule=link_w_with_x_and_room_rule2,
+    )
+
+    def link_w_with_x_and_room_rule3(model, c, t, room):
+        return model.w[c, t, room] >= model.x[c, t] + model.room_assignment[c, room] - 1
+
+    model.link_w_with_x_and_room3 = Constraint(
+        model.Classes,
+        model.TimeSlots,
+        model.Classrooms,
+        rule=link_w_with_x_and_room_rule3,
+    )
+
+    # 6. Force specific classes into specific time slots
     if "ForcedAssignments" in data and data["ForcedAssignments"]:
 
         def force_assignment_rule(model, c, t):
@@ -119,23 +270,80 @@ def create_and_solve_timetable_model(data):
             model.Classes, model.TimeSlots, rule=force_assignment_rule
         )
 
-    # Objective: Minimize the total number of time slots used
-    def dummy_objective_rule(model):
-        return 0  # A feasible solution is the main goal
+    # Link x and y: y[e, t] should be 1 if any class taught by teacher e is at time slot t
+    def link_x_y_rule(model, e, t):
+        return sum(model.v[c, t, e] for c in model.Classes) >= model.y[e, t]
 
-    model.objective = Objective(rule=dummy_objective_rule)
+    model.link_x_y = Constraint(model.Teachers, model.TimeSlots, rule=link_x_y_rule)
+
+    # Link v with x and teacher_assignment
+    def link_v_with_x_and_teacher_rule1(model, c, t, teacher):
+        return model.v[c, t, teacher] <= model.x[c, t]
+
+    model.link_v_with_x_and_teacher1 = Constraint(
+        model.Classes,
+        model.TimeSlots,
+        model.Teachers,
+        rule=link_v_with_x_and_teacher_rule1,
+    )
+
+    def link_v_with_x_and_teacher_rule2(model, c, t, teacher):
+        return model.v[c, t, teacher] <= model.teacher_assignment[c, teacher]
+
+    model.link_v_with_x_and_teacher2 = Constraint(
+        model.Classes,
+        model.TimeSlots,
+        model.Teachers,
+        rule=link_v_with_x_and_teacher_rule2,
+    )
+
+    def link_v_with_x_and_teacher_rule3(model, c, t, teacher):
+        return (
+            model.v[c, t, teacher]
+            >= model.x[c, t] + model.teacher_assignment[c, teacher] - 1
+        )
+
+    model.link_v_with_x_and_teacher3 = Constraint(
+        model.Classes,
+        model.TimeSlots,
+        model.Teachers,
+        rule=link_v_with_x_and_teacher_rule3,
+    )
+
+    # Ensure first and last slots are correctly identified
+    def min_first_time_slot_rule(model, e, t):
+        return model.first[e] <= t + (1 - model.y[e, t]) * len(model.TimeSlots)
+
+    model.min_first_time_slot = Constraint(
+        model.Teachers, model.TimeSlots, rule=min_first_time_slot_rule
+    )
+
+    def max_last_time_slot_rule(model, e, t):
+        return model.last[e] >= t - (1 - model.y[e, t]) * len(model.TimeSlots)
+
+    model.max_last_time_slot = Constraint(
+        model.Teachers, model.TimeSlots, rule=max_last_time_slot_rule
+    )
+
+    # Objective: Minimize the working span for each teacher
+    def minimize_teacher_working_span(model):
+        return sum(model.last[e] - model.first[e] for e in model.Teachers)
+
+    model.objective = Objective(rule=minimize_teacher_working_span, sense=minimize)
 
     # Solve the model
-    solver = SolverFactory("glpk")
+    solver = Highs()
     solver.solve(model)
 
+    # Calculate the total cost
+    teacher_cost_per_hour = 100
+    total_cost = sum(
+        (model.last[e].value - model.first[e].value + 1) * teacher_cost_per_hour
+        for e in model.Teachers
+    )
+
     # Display results in a terminal table grid
-    print("Class Timetable:")
-
-    # Create a PrettyTable object
     room_table = PrettyTable()
-
-    # Dynamically add room columns based on the number of classrooms
     room_names = sorted(list(model.Classrooms))
     room_table.field_names = ["Time Slot"] + room_names
 
@@ -145,17 +353,16 @@ def create_and_solve_timetable_model(data):
         for room in room_names:
             room_class = ""
             for c in model.Classes:
-                if model.x[c, t].value == 1 and class_room[c] == room:
+                if (
+                    model.x[c, t].value == 1
+                    and model.room_assignment[c, room].value == 1
+                ):
                     room_class = c
                     break
             row.append(room_class)
         room_table.add_row(row)
 
-    print(room_table)
-
     teacher_table = PrettyTable()
-
-    # Dynamically add room columns based on the number of classrooms
     teacher_table.field_names = ["Time Slot"] + room_names
 
     # Populate the table with teacher schedules
@@ -164,47 +371,45 @@ def create_and_solve_timetable_model(data):
         for room in room_names:
             room_teacher = ""
             for c in model.Classes:
-                if model.x[c, t].value == 1 and class_room[c] == room:
-                    room_teacher = class_teacher[c]
+                if (
+                    model.x[c, t].value == 1
+                    and model.room_assignment[c, room].value == 1
+                ):
+                    room_teacher = [
+                        teacher
+                        for teacher in model.Teachers
+                        if model.teacher_assignment[c, teacher].value == 1
+                    ][0]
                     break
             row.append(room_teacher)
         teacher_table.add_row(row)
 
-    print(teacher_table)
     return {
         "room_table": room_table.get_string(),
         "teacher_table": teacher_table.get_string(),
+        "total_cost": total_cost,
     }
 
 
 # Example usage with the updated data structure:
 if __name__ == "__main__":
     data = {
-        "Classes": ["C1", "C2", "C3"],
-        "TimeSlots": ["T1", "T2", "T3"],
-        "Teachers": ["Teacher1", "Teacher2"],
-        "Classrooms": ["Room1", "Room2"],
-        "Teachers": ["Teacher1", "Teacher2"],
-        "ClassTeacherMapping": [
-            {"classId": "C1", "teacher": "Teacher1"},
-            {"classId": "C2", "teacher": "Teacher2"},
-            {"classId": "C3", "teacher": "Teacher1"},
-        ],
-        "ClassRoomMapping": [
-            {"classId": "C1", "room": "Room1"},
-            {"classId": "C2", "room": "Room1"},
-            {"classId": "C3", "room": "Room2"},
-        ],
+        "Classes": ["C1", "C2", "C3", "C4", "C5", "C6", "C7"],
+        "TimeSlots": ["T1", "T2", "T3", "T4", "T5", "T6", "T7"],
+        "Teachers": ["Teacher1", "Teacher2", "Teacher3", "Teacher4", "Teacher5"],
+        "Classrooms": ["Room1", "Room2", "Room3", "Room4"],
         "Constraints": {
             "OneTimeSlotPerClass": True,
             "TeacherConflict": True,
             "RoomConflict": True,
         },
         "ForcedAssignments": [
-            {"classId": "C1", "timeslot": "T1"},  # Force C1 to be in T1
-            {"classId": "C2", "timeslot": "T2"},  # Force C2 to be in T2
+            # {"classId": "C1", "timeslot": "T1"},  # Force C1 to be in T1
+            # {"classId": "C2", "timeslot": "T2"},  # Force C2 to be in T2
         ],
     }
 
     # Call the function with the data
-    create_and_solve_timetable_model(data)
+    res = create_and_solve_timetable_model(data)
+    print(res["teacher_table"])
+    print(res)
